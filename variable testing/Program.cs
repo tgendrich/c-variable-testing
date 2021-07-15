bfb5e31ec1bfb70cc3e7070d747154310da4df57
tgendrich <gendricht@gmail.com>
2021-07-15 14:38:29 -0500

    start

---
+++ variable testing/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace variable_testing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string Newstring = "my name is Thomas";
+            bool Istrue = false;
+            char A = 'a';
+            int B = 1;
+            double C = 2.22;
+            decimal D = 4m;
+            Console.WriteLine($"{Newstring}, and it is {Istrue} that I am a cat who is {B} years old");
+            string Wholeattempt = Newstring + " and it is " + Istrue + " that I am a cat who is " + B + "years old";
+            Console.WriteLine(Wholeattempt);
+        }
+    }
+}
